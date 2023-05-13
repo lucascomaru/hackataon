b95be5708b91eba5893d1f31a77987332b800c27
Lucas <lucascomaru20@gmail.com>
2023-05-13 10:51:53 -0300

    Repaginando o projeto!

--- main.py
+++ main.py
@@ -3,26 +3,12 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/') #Atribui uma nova funcionalidade para a função abaixo, faz a função abaixo aparecer no site
-def logins():
-    return render_template('logins.html')
-
-@app.route('/criar_conta')
-def criar_conta():
-    return render_template('criar_conta.html')
-@app.route('/rh')
-def rh():
-    return render_template('rh.html')
-@app.route('/colaborador')
-def colaborador():
-    return render_template('colaborador.html')
-@app.route('/professor')
-def professor():
-    return render_template('professor.html')
+def painel():
+    return render_template('painel.html')
 
 @app.route('/adicionar_treinamento')
 def adicionar_treinamento():
     return render_template('adicionar_treinamento.html')
-
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
