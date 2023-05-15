52ba5d74a6fa3d7b1b47f42a3934d9121ad6926d
Lucas <lucascomaru20@gmail.com>
2023-05-15 16:35:57 -0300

    Reformulando

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mdbtreinamentos.db'
 
@@ -12,12 +12,17 @@ database = SQLAlchemy(app)
 def painel():
     return render_template('painel.html')
 
-@app.route('/adicionar_treinamento')
-def adicionar_treinamento():
-    return render_template('adicionar_treinamento.html')
+@app.route('/treinamentos')
+def treinamentos():
+    return render_template('treinamentos.html')
+
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
+
+@app.route('/sessao-download')
+def sessao_download():
+    return render_template('sessao-download.html')
 
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -2,107 +2,37 @@ function exibirConteudo(area, botao) {
     var conteudo = document.getElementById('conteudo');
 
     if (area === 'painel') {
-        conteudo.innerHTML = '<div class="conteudo-centralizado"><h2>Conteúdo do Painel</h2></div>';
+        fetch('/painel')
+            .then(response => response.text())
+            .then(html => {
+                conteudo.innerHTML = html;
+                botao.classList.add('highlight');
+            })
+            .catch(error => console.log(error));
     } else if (area === 'treinamentos') {
-        conteudo.innerHTML = `
-            <div class="conteudo-centralizado">
-                <h2>Conteúdo dos Treinamentos</h2>
-                <div class="treinamento-form">
-                    <div class="form-group">
-                        <label for="nome-treinamento">Nome do Treinamento:</label>
-                        <div class="input-wrapper">
-                            <input type="text" name="nome-treinamento" id="nome-treinamento" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="nome-professor">Nome do Professor:</label>
-                        <div class="input-wrapper">
-                            <input type="text" name="nome-professor" id="nome-professor" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="setor-treinamento">Setor do Treinamento:</label>
-                        <div class="input-wrapper">
-                            <select name="setor-treinamento" id="setor-treinamento" required>
-                                <option value="">Selecione um setor</option>
-                                <option value="Produção">Produção</option>
-                                <option value="Logística">Logística</option>
-                                <option value="Armazenamento">Armazenamento</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="data-treinamento">Data do Treinamento:</label>
-                        <div class="input-wrapper">
-                            <input type="date" name="data-treinamento" id="data-treinamento" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="hora-treinamento">Hora do Treinamento:</label>
-                        <div class="input-wrapper">
-                            <input type="time" name="hora-treinamento" id="hora-treinamento" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <input type="submit" value="Salvar">
-                    </div>
-                </div>
-            </div>`;
+        fetch('/treinamentos')
+            .then(response => response.text())
+            .then(html => {
+                conteudo.innerHTML = html;
+                botao.classList.add('highlight');
+            })
+            .catch(error => console.log(error));
     } else if (area === 'cadastro') {
-        conteudo.innerHTML = `
-            <div class="conteudo-centralizado">
-                <h2>Conteúdo do Cadastro</h2>
-                <form action="/cadastro" method="POST" class="cadastro-form">
-                    <div class="form-group">
-                        <label for="nome">Nome:</label>
-                        <div class="input-wrapper">
-                            <input type="text" name="nome" id="nome" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="re">RE:</label>
-                        <div class="input-wrapper">
-                            <input type="text" name="re" id="re" required>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="setor">Setor:</label>
-                        <div class="input-wrapper">
-                            <select name="setor" id="setor" required>
-                                <option value="">Selecione um setor</option>
-                                <option value="Produção">Produção</option>
-                                <option value="Logística">Logística</option>
-                                <option value="Armazenamento">Armazenamento</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="cargo">Cargo:</label>
-                        <div class="input-wrapper">
-                            <select name="cargo" id="cargo" required>
-                                <option value="">Selecione um cargo</option>
-                                <option value="Auxiliar de Produção">Auxiliar de Produção</option>
-                                <option value="Conferente">Conferente</option>
-                                <option value="Estoquista">Estoquista</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <input type="submit" value="Cadastrar">
-                    </div>
-                </form>
-            </div>`;
+        fetch('/cadastro')
+            .then(response => response.text())
+            .then(html => {
+                conteudo.innerHTML = html;
+                botao.classList.add('highlight');
+            })
+            .catch(error => console.log(error));
     } else if (area === 'sessao-download') {
-        conteudo.innerHTML = '<div class="conteudo-centralizado"><h2>Conteúdo da Sessão de Download</h2></div>';
+        fetch('/sessao-download')
+            .then(response => response.text())
+            .then(html => {
+                conteudo.innerHTML = html;
+                botao.classList.add('highlight');
+            })
+            .catch(error => console.log(error));
     }
 
     var botoes = document.getElementsByClassName('button');
@@ -111,4 +41,3 @@ function exibirConteudo(area, botao) {
     }
     botao.classList.add('highlight');
 }
-
