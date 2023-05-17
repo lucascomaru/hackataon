b6fb272c5157309456301e5ef0454b0e65383c31
Lucas <lucascomaru20@gmail.com>
2023-05-17 00:32:21 -0300

    Algumas atualizações

--- mdbtreinamentos/routes.py
+++ mdbtreinamentos/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request, send_file
 from mdbtreinamentos import app, database
 from mdbtreinamentos.models import Treinamento, Pessoa, Cadastro
 from mdbtreinamentos.forms import TreinamentoForm, CadastroForm
@@ -15,6 +15,12 @@ def painel():
     treinamentos = Treinamento.query.all()
     return render_template('painel.html', treinamentos=treinamentos)
 
+@app.route('/detalhes_treinamento/<int:treinamento_id>', methods=['GET'])
+def detalhes_treinamento(treinamento_id):
+    treinamento = Treinamento.query.get_or_404(treinamento_id)
+    return render_template('detalhes_treinamento.html', treinamento=treinamento)
+
+
 @app.route('/treinamentos', methods=['GET', 'POST'])
 def treinamentos():
     form = TreinamentoForm()
@@ -22,19 +28,20 @@ def treinamentos():
         nome_treinamento = form.nome_treinamento.data
         nome_professor = form.nome_professor.data
         setor_treinamento = form.setor_treinamento.data
+        sala_treinamento = form.sala_treinamento.data
         data_treinamento = form.data_treinamento.data
         hora_treinamento = form.hora_treinamento.data
         tipo_treinamento = form.tipo_treinamento.data
 
         treinamento = Treinamento(nome=nome_treinamento, professor=nome_professor, setor=setor_treinamento,
-                                  data=data_treinamento, hora=hora_treinamento, tipo=tipo_treinamento)
+                                  sala=sala_treinamento, data=data_treinamento, hora=hora_treinamento, tipo=tipo_treinamento)
 
         database.session.add(treinamento)
         database.session.commit()
 
         flash('Treinamento criado com sucesso!', 'success')
 
-        return redirect(url_for('treinamentos'))
+        return redirect(url_for('homepage'))
 
     return render_template('treinamentos.html', form=form)
 
@@ -68,6 +75,8 @@ def cadastro():
 
     return render_template('cadastro.html', form=form)
 
+
+
 def atualizar_excel_frequencia(treinamento):
 
     cadastros = Cadastro.query.filter_by(treinamento_id=treinamento.id).all()
@@ -92,4 +101,5 @@ def atualizar_excel_frequencia(treinamento):
     df.to_excel(caminho_arquivo, index=False)
 
     print(f"Arquivo Excel salvo em: {caminho_arquivo}")
+
 
